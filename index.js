f3fce0eae5d3a2d4a27c076707d32f7937d34a4c
ingabiremariesandrine187 <ingabiremariesandrine187@gmail.com>
2025-10-03 09:36:08 +0200

    new changes

--- index.js
+++ index.js
@@ -1,31 +1,69 @@
-async function fetchDataForAllUsers() {
-    try{
+// async function fetchDataForAllUsers() {
+//     try{
     
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_embed=comments&_expand=user`);
+//     const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_embed=comments&_expand=user`);
 
     
-    if(!response.ok){
-     throw new Error(`HTTP error! status: ${response.status}`)
-    }
-    //parse the json response
-    const data = await response.json();
-    return data
-} catch(error){
-    console.log("Error fetching user data: ",error)
-}
-}
-// displaying all users along with their ids,names and emails
-async function displayAllUsers(){
-    const users = await fetchDataForAllUsers()
-    if(users && users.length>0){
-    users.forEach(user => {
-        console.log("user id:", user.id)
-         console.log("user name:",user.name)
-         console.log("articleIds",user.articleIds)
-    });
-} else{
-    console.log("no user found")
-}
-}
-displayAllUsers();
+//     if(!response.ok){
+//      throw new Error(`HTTP error! status: ${response.status}`)
+//     }
+//     //parse the json response
+//     const data = await response.json();
+//     return data
+// } catch(error){
+//     console.log("Error fetching user data: ",error)
+// }
+// }
+
+// async function displayAllUsers(){
+//     const users = await fetchDataForAllUsers()
+//     if(users && users.length>0){
+//     users.forEach(user => {
+//         console.log("user id:", user.id)
+//          console.log("user name:",user.name)
+//          console.log("articleIds",user.articleIds)
+//              console.log("content",user.content)
+//     });
+// } else{
+//     console.log("no user found")
+// }
+// }
+// displayAllUsers();
     
+
+
+async function fetchDataForAllUsers() {
+  try {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts?_embed=comments&_expand=user`
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log("Error fetching user data: ", error);
+  }
+}
+
+async function displayAllUsers() {
+  const posts = await fetchDataForAllUsers();
+
+  if (posts && posts.length > 0) {
+    posts.forEach((post) => {
+      console.log("Post id:", post.id);                  
+      console.log("Author id:", post.user.id);           
+      console.log("Author name:", post.user.name);     
+      console.log("Article (title):", post.title);       
+      console.log("Comments:", post.comments.length);    
+      console.log("----");
+    });
+  } else {
+    console.log("No posts found");
+  }
+}
+
+displayAllUsers();
